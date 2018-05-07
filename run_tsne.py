64bf65cd6dd8a84fdbf4c56e4e56063b28f49ac7
vuvietmi <vuvietminh@unamur.be>
2018-05-07 09:20:03 +0000

    Run t-sne in parallel with different perplexities

---
+++ run_tsne.py
@@ -0,0 +1,37 @@
+# run tsne for different datasets with different perplexity
+
+import time
+from joblib import Parallel, delayed, dump
+from sklearn.manifold import TSNE
+
+from utils import gen_log_space_float
+from dataset_utils import load_dataset
+
+import multiprocessing
+n_cpus = multiprocessing.cpu_count()
+n_cpus_using = int(0.75 * n_cpus)
+
+out_dir = './output'
+
+def _run_tsne(X, perp, i):
+    print('[i={}] perp={}'.format(i, perp))
+    tsne = TSNE(perplexity=perp, random_state=0, verbose=0)
+    tsne.fit_transform(X)
+    out_name = '{}/{}/{}.z'.format(out_dir, dataset_name, i)
+    dump(tsne, out_name)
+
+def run_embedding(dataset_name):
+    X, y, _ = load_dataset(dataset_name)
+    N = X.shape[0]
+    perps = gen_log_space_float(limit=N, n=N//2)
+    print('Number of perps: ', len(perps))
+    
+    Parallel(n_jobs=n_cpus_using)(
+        delayed(_run_tsne)(X, perp, i) for i, perp in enumerate(perps)
+    )
+
+
+if __name__ == '__main__':
+    dataset_name = 'MNIST-SMALL'
+    run_embedding(dataset_name)
+
