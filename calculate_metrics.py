36ed876d520ccec8ca510480a77da6abe5c63fc9
minh@xlap2 <vuvietminh@unamur.be>
2018-05-08 17:28:15 +0200

    Refactor  to run in parallel

--- calculate_metrics.py
+++ calculate_metrics.py
@@ -1,23 +1,28 @@
 # calculate quality metric scores
 
-import joblib
 import os
-
+from joblib import Parallel, delayed, load
 
 from metrics import DRMetric
 from dataset_utils import load_dataset
 import db_utils
 
+import multiprocessing
+n_cpus = multiprocessing.cpu_count()
+n_cpus_using = int(0.75 * n_cpus)
 
 input_folder = './input'
 output_folder = './output'
+key_name_in_DB = 'metrics'
 
 
-def _calculate_metrics(X_original, tsne_obj, metric_names):
+def _calculate_metrics(db_name, X_original, tsne_file, metric_names):
     """ Util function that calls `DRMetric` to do the calculation
     """
+    print('Processing: ', tsne_file)
 
     # extract fields from pre-calculated tsne object
+    tsne_obj = load(tsne_file)
     X_2d = tsne_obj.embedding_
     loss = tsne_obj.kl_divergence_
     n_iter = tsne_obj.n_iter_
@@ -38,17 +43,15 @@ def _calculate_metrics(X_original, tsne_obj, metric_names):
         metric_method = getattr(drMetric, metric_name)
         record[metric_name] = metric_method()
 
-    return record
+    # save the result in `record` to database
+    db_utils.append_to_db(db_name, key_name_in_DB, records=[record])
 
 
 def calculate_metric(dataset_name):
-    """ Util function that manages all steps to calculate metric score
-        for each given dataset
+    """ Util function that manages to calculate metric scores
+        for each given dataset (in parallel)
     """
-
-    # prepare database and key to store the result
     db_name = 'DB_{}'.format(dataset_name)
-    key_name = 'metrics'
 
     # prepare a list of metric names:
     metric_names = [
@@ -60,27 +63,30 @@ def calculate_metric(dataset_name):
     ]
 
     # load original dataset
-    X_original, y, target_labels = load_dataset(dataset_name)
+    X_original, _, _ = load_dataset(dataset_name)
 
-    # prepare a loop to load all pre-defined tsne objects
+    # prepare a list of pre-calculated tsne object files
+    to_process_files = []
     embedding_dir = '{}/{}'.format(output_folder, dataset_name)
     for file in os.listdir(embedding_dir):
         if file.endswith('.z'):
             in_name = os.path.join(embedding_dir, file)
-            print('Processing: ', in_name)
+            to_process_files.append(in_name)
+    print('{} files to process'.format(len(to_process_files)))
 
-            tsne_obj = joblib.load(in_name)
-            record = _calculate_metrics(X_original, tsne_obj, metric_names)
-
-            # save the calculated metric scores to HDFStore
-            db_utils.append_to_db(db_name, key_name, records=[record])
+    # setup to run calculation in parallel
+    Parallel(n_jobs=n_cpus_using)(
+        delayed(_calculate_metrics)(
+            db_name, X_original, tsne_file, metric_names
+        ) for tsne_file in to_process_files
+    )
 
 
 if __name__ == '__main__':
     # if run code in sequential model, disable parallel flag
     db_utils.IS_PARALLEL = True
 
-    dataset_name = 'COIL20'
+    dataset_name = 'MNIST-SMALL'
     db_name = 'DB_{}'.format(dataset_name)
 
     calculate_metric(dataset_name)
